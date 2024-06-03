30d814bd93dc3f575be8f6189bf30716e92c1f9a
Crowdbotics <team@crowdbotics.com>
2024-06-03 00:09:28 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "black-tree-48222",
+  url: "https://black-tree-48222.botics.co",
+  api: "https://black-tree-48222.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
